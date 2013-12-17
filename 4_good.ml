e7943685e63cea4cf212a888b3f94b5209eb8dad
Jack Chen <massd.me@gmail.com>
2013-12-17 23:00:51 +0000

    added good problem 4

--- 4_good.ml
+++ 4_good.ml
@@ -1,3 +1,11 @@
+(**
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+
+*)
+
 let rev_int n = 
   let rec rev acc n =
     if n = 0 then acc
